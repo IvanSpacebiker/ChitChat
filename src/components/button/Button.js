f32067a22eb2294627aacbd66dab74ee8d055474
IvanSpacebiker <kii1234@yandex.ru>
2023-01-30 15:30:23 +0300

    Make button component as button

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,19 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+//Styles
+import "./Button.scss"
+
 
 function Button(props) {
+
     return (
-        <div>Button</div>
+        <div className={"button type_" + props.type} onClick={props.onClick}>
+            <p>{props.label}</p>
+        </div>
     );
 }
 
 Button.propTypes = {
     label   : PropTypes.string.isRequired,
-    type    : PropTypes.string.isRequired,
+    type    : PropTypes.number.isRequired,
 
     // Handlers
     onClick : PropTypes.func.isRequired,
 };
+
+
+
+
+
+
 
 export default Button;
